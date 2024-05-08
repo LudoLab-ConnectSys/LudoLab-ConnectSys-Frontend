919573d80cda984b4c6da9ddc187f1df9a665a88
luischiliguano <95940533+luischiliguano@users.noreply.github.com>
2024-05-07 20:18:13 -0500

    Creacion componente cursos

---
+++ Areas/Administracion/Services/Disponibilidad.cs
@@ -0,0 +1,9 @@
+﻿namespace LudoLab_ConnectSys_Frontend.Areas.Administracion.Services
+{
+    public class Disponibilidad
+    {
+        public int Dia { get; set; }
+        public string HoraInicio { get; set; }
+        public string HoraFin { get; set; }
+    }
+}
---
+++ Areas/Administracion/Services/IAgendable.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace LudoLab_ConnectSys_Frontend.Areas.Administracion.Services
+{
+    public interface IAgendable
+    {
+        List<Disponibilidad> HorariosDisponibles { get; set; }
+
+    }
+}
