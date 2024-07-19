e3ff19325f0b3472ce3ab16982a5f8f32009b1e7
GINNO TAIMAL <ginno.taimal@epn.edu.ec>
2024-07-18 21:58:38 -0500

    feat(auth): Lectura de JWT para auth

---
+++ Areas/Principal/Services/JwtAuthenticationStateProvider.cs
@@ -0,0 +1,69 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace LudoLab_ConnectSys_Frontend.Areas.Principal.Services;
+
+public class JwtAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly ILocalStorageService _localStorageService;
+    private readonly HttpClient _httpClient;
+
+    public JwtAuthenticationStateProvider(ILocalStorageService localStorageService, HttpClient httpClient)
+    {
+        _localStorageService = localStorageService;
+        _httpClient = httpClient;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var token = await _localStorageService.GetItemAsync<string>("authToken");
+
+        var identity = string.IsNullOrEmpty(token)
+            ? new ClaimsIdentity()
+            : new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+
+        var user = new ClaimsPrincipal(identity);
+
+        _httpClient.DefaultRequestHeaders.Authorization = token != null
+            ? new AuthenticationHeaderValue("Bearer", token)
+            : null;
+
+        return new AuthenticationState(user);
+    }
+
+    public void NotifyUserAuthentication(string token)
+    {
+        var claims = ParseClaimsFromJwt(token);
+        var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+        var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+        NotifyAuthenticationStateChanged(authState);
+
+        // Extraer y mostrar información del usuario
+        var userName = claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+        var userRole = claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+        Console.WriteLine($"Usuario: {userName}, Rol: {userRole}");
+    }
+
+    public void NotifyUserLogout()
+    {
+        var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+        var authState = Task.FromResult(new AuthenticationState(anonymousUser));
+        NotifyAuthenticationStateChanged(authState);
+    }
+
+    private bool CanReadToken(string token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        return handler.CanReadToken(token);
+    }
+
+    private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.ReadJwtToken(jwt);
+        return token.Claims;
+    }
+}
