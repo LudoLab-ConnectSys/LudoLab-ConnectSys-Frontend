124f0015817ec075616811feae65b19745955e3a
Ginno Taimal <ginno.taimal@epn.edu.ec>
2024-08-19 22:51:22 -0500

    feat: Centralizar los path de microservicios
    Se centralizo en un solo lugar los path de los microservicios enfocados
    en la seguridad desarrollados por Ginno

--- Areas/Principal/Models/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LudoLab_ConnectSys_Frontend.Areas.Principal.Models;
-
-public class LoginRequest
-{
-    public string Identification { get; set; }
-    public string Password { get; set; }
-    public string Rol { get; set; }
-}
