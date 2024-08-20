c07a19024c425a0f120f6831e45c150be2c29f66
Ginno Taimal <ginno.taimal@epn.edu.ec>
2024-08-19 22:59:31 -0500

    feat: Se instancio los nuevos servicios

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Blazored.LocalStorage;
 using CurrieTechnologies.Razor.SweetAlert2;
 using LudoLab_ConnectSys_Frontend;
+using LudoLab_ConnectSys_Frontend.Services.Auditoria;
+using LudoLab_ConnectSys_Frontend.Services.Registro;
+using LudoLab_ConnectSys_Frontend.Services.Security;
 using LudoLab_ConnectSys_Frontend.Shared.Utilities;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
@@ -13,6 +17,23 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Para usar LocalStorage
+builder.Services.AddBlazoredLocalStorage();
+
+// Registrar el servicio de AuditLog
+builder.Services.AddScoped<AuditLogService>();
+
+// Mandor en el header el idUsuario y nombreUsuario
+// Agregar el CustomHttpClientHandler
+builder.Services.AddTransient<CustomHttpClientHandler>();
+
+// Configurar HttpClient para usar CustomHttpClientHandler
+builder.Services.AddHttpClient("AuthenticatedClient")
+    .AddHttpMessageHandler<CustomHttpClientHandler>();
+
+// Registrar AuthService usando la interfaz IAuthService
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IRegistroService, RegistroService>();
 
 // Configuraci�n del HttpClient para interactuar con tu backend
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
